b1c2ec1d1427d37a43ff6f5f45723ca83ca8c2ca
hazem <hazemmh@gmail.com>
2024-03-21 03:14:35 +0200

    alphanum

--- source/modules/booking/booking.validation.js
+++ source/modules/booking/booking.validation.js
@@ -6,8 +6,8 @@ import { generalFiled } from "../../utilits/generalfields.js";
 export const createbooking= {
     body: joi.object().keys({
         email:generalFiled.email,
-        firstname: joi.string().min(2).max(15).alphanum().required(),
-        lastname: joi.string().min(2).max(15).alphanum().required(),
+        firstname: joi.string().min(2).max(15).required(),
+        lastname: joi.string().min(2).max(15).required(),
         phone:generalFiled.phone.required(),
         address:generalFiled.address,
     }).required(),
