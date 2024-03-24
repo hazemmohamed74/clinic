94448b767920d30253c6796dced6a378ef5c10a3
AbdallaM-Ibrahim <Abdalla.mostafa19200@gmail.com>
2024-03-24 18:05:45 +0200

    fix bug

--- source/modules/booking/booking.controllers.js
+++ source/modules/booking/booking.controllers.js
@@ -5,16 +5,13 @@ import appointmentModel from "../../../DB/models/appointmentModel.js";
 //**************************createbooking*************************** */
 export const createbooking = asyncHandler(async(req,res,next)=>{
     const {firstname,lastname,email,address,phone} = req.body
-    // const bookingExist = await appointmentModel.findOne({firstname, phone})
-    if (!bookingExist) {
-        const booking = await appointmentModel.create( {
-            firstname,
-            lastname,
-            email,
-            address,
-            phone
-        })
-        res.json({ msg: "done", booking })
-    }
-    booking ? res.json({ msg: "done", booking }) : next(new AppError("fail", 500))
+    
+    const booking = await appointmentModel.create( {
+        firstname,
+        lastname,
+        email,
+        address,
+        phone
+    })
+    res.json({ msg: "done", booking })
 })
